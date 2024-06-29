cecec310ef549cde3f0d53191a2cddfc8d49fa19
Yash <yash@Yashs-MacBook-Pro.local>
2024-06-29 21:26:31 +0530

    wip

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 
 export default function Home() {
   return (
-    <main className="h-screen flex flex-col gap-2">
-      <Button>Hello world</Button>
+    <main className="h-screen flex flex-col gap-2 px-16 py-12 items-center zxcsdvsdv">
+      <Input type="text" className="outline-none focus:outline-none" />
+      <Button variant={"ghost"} size={"sm"} className="w-fit">
+        search
+      </Button>
     </main>
   );
 }
